cad89752000582f65c161c6b0e713681aa140edf
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-06-15 20:30:52 +0200

    Server Express

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.json({message: "Bienvenidos a la API de Hygge Coffee"});
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
