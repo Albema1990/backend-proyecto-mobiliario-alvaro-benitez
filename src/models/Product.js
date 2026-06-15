c9378baf198c1079e494501581f8f6d52275927e
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-06-15 22:03:59 +0200

    seeder product

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,43 +1,60 @@
 import mongoose, { mongo } from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-  description: {
-    type: String,
-    default: "",
-  },
+    description: {
+      type: String,
+      default: "",
+    },
 
-  category: {
-    type: String,
-    required: true,
-    enum: ["cafe", "te", "accesorios"],
-  },
+    category: {
+      type: String,
+      required: true,
+      enum: ["Café", "Té", "Accesorios"],
+    },
 
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
 
-  image: {
-    type: String,
-    default: "",
-  },
+    image: {
+      type: String,
+      default: "",
+    },
 
-  onSale: {
-    type: Boolean,
-    default: false,
+    onSale: {
+      type: Boolean,
+      default: false,
+    },
+
+    origin: {
+      type: String,
+      default: "",
+    },
+
+    roast: {
+      type: String,
+      default: "",
+    },
+
+    weight: {
+      type: String,
+      default: "",
+    },
   },
-},
-{
+  {
     timestamps: true,
-});
+  },
+);
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 export default Product;
