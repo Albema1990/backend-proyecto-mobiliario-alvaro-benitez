7622bff9e4050cfb00c8cde2f2c38dc6144f84bf
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-06-16 21:36:07 +0200

    PUT product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,25 +3,19 @@
 // Controller para crear un producto
 
 export const createProduct = async (req, res) => {
-    try {
-        const { 
-            name,
-            category,
-            price,
-        } = req.body;
+  try {
+    const { name, category, price } = req.body;
 
-        if (!name || !category || !price) {
-            return res
-            .status(422)
-            .json({message: "Todos los campos son obligatorios"});
-        } 
-
-        const product = await Product.create(req.body);
-
-        res.status(201).json(product);
-    } catch (error) {
-
+    if (!name || !category || !price) {
+      return res
+        .status(422)
+        .json({ message: "Todos los campos son obligatorios" });
     }
+
+    const product = await Product.create(req.body);
+
+    res.status(201).json(product);
+  } catch (error) {}
 };
 
 // Controller para obtener todos los productos
@@ -29,33 +23,46 @@ export const createProduct = async (req, res) => {
 import Product from "../models/product.js";
 
 export const getProducts = async (req, res) => {
-    try {
-        const products = await Product.find().select("-description -__v");
-        res.json(products);
-        
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: "Error al obtener los productos"});
-    }
+  try {
+    const products = await Product.find().select("-description -__v");
+    res.json(products);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Error al obtener los productos" });
+  }
 };
 
 //Controller para obtener un producto por su ID
 
 export const getProductById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+
+    res.json(product);
+  } catch (error) {
+    // console.log(error.message);
+
+    res.status(500).json({ message: "Error al obtener el producto" });
+  }
+};
+
+// Controller para actualizar un producto por su ID
+
+export const updateProduct = async (req, res) => {
+  try {
     const { id } = req.params;
 
-    try {
-        const product = await Product.findById(id);
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-        if(!product) {
-            return res.status(404).json({ message: "Producto no encontrado" });
-        }
-
-        res.json(product);
-
-    } catch (error) {
-        // console.log(error.message);
-
-        res.status(500).json({message: "Error al obtener el producto"});
-    }
-}
+    res.json(product);
+  } catch (error) {}
+};
--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -3,7 +3,8 @@ import { Router } from "express";
 import {
   getProducts,
   getProductById,
-  createProduct
+  createProduct,
+  updateProduct
 } from "../controllers/product.controller.js";
 
 const router = Router();
@@ -14,5 +15,7 @@ router.post("/", createProduct);
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
+
+router.put("/:id", updateProduct);
 
 export default router;
